5c1dbdbaea2c8c3961606bc6e039347d4d5c719f
Oleksandr <education.oleksandr@gmail.com>
2022-09-15 16:56:36 +0300

    aa

--- tasks/task1/abonent.h
+++ tasks/task1/abonent.h
@@ -11,23 +11,44 @@
 
 
 class Abonent {
-    std::string name, surname, address;
-    Date date_of_reg, date_of_last_check;
-    Date all_dates[10000];
-    bool arrears;
-    double rate;
-    int standard = 3;
+    private:
+        std::string name, surname, address;
+        Date date_of_reg, date_of_last_check;
+        Date *all_dates;
+        int size;
+        bool arrears;
+        double rate;
+        int standard = 3;
     public:
+        Abonent();
         Abonent(    bool arrears_,
                     double rate_,
                     std::string address,
                     Date date_of_reg_,
                     Date date_of_last_check_,
-                    Date all_dates_
+                    Date *all_dates_
                 );
+        Abonent(const Abonent& other);
+
+        void set_name(std::string name_) {
+            name = name_;
+        }
+        void set_surname(std::string surname_) {
+            surname = surname_;
+        }
+        void set_address(std::string address_) {
+            address = address_;
+        }
+        void set_date_of_reg(int day, int month, int year) {
+            date_of_reg = Date(day, month, year);
+        }
+        void set_date_of_last_check(int day, int month, int year) {
+            date_of_reg = Date(day, month, year);
+        }
+
         int service_costs(){
             // return rate ? if 
         }
-};
+};  
 
 #endif
